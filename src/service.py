32681b9e38d0e2495a010c81a72eb5461cdc688d
RafaelWO <weingartner.rafael@hotmail.com>
2021-01-09 17:56:12 +0100

    Enabled basic conversation

--- src/service.py
+++ src/service.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
-from flask_restx import Resource, Api, fields, namespace
-from transformers import Conversation, pipeline, AutoTokenizer, AutoModelForCausalLM
+from flask_restx import Resource, Api, fields
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM, AutoConfig
+from transformers import Conversation as ConversationHelper
 
 app = Flask(__name__)
 api = Api(app, version="0.1", title="AI Conversation")
@@ -16,9 +17,17 @@ conversation_input_def = api.model("Conversation Input", {
 
 
 model_str = "microsoft/DialoGPT-medium"
-tokenizer = AutoTokenizer.from_pretrained(f"local_model/{model_str}/tokenizer")
+print(f"Using model '{model_str}'")
+print("Loading tokenizer...", end=' ')
+tokenizer = AutoTokenizer.from_pretrained(f"local_model/{model_str}/tokenizer",
+                                          config=AutoConfig.from_pretrained(f"local_model/{model_str}/model"))
+print("Done")
+print("Loading model...", end=' ')
 model = AutoModelForCausalLM.from_pretrained(f"local_model/{model_str}/model")
+print("Done")
+print("Building pipeline...", end=' ')
 conv_pipe = pipeline("conversational", model=model, tokenizer=tokenizer)
+print("Done")
 
 
 @ns.route('/conversation')
@@ -26,11 +35,11 @@ class Conversation(Resource):
     @ns.expect(conversation_input_def)
     def post(self):
         input_text = request.json['text']
-        # conversation = Conversation(input_text)
-        # out = conv_pipe(conversation)
+        conversation = ConversationHelper(input_text)
+        out = conv_pipe(conversation)
 
-        return {'entered': input_text}
+        return {'conversation': repr(out)}
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
